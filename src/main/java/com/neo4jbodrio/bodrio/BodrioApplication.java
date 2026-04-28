a055ed59f129464101a617d2a1d4fd5bcc359451
Aday <adaysg14@gmail.com>
2026-04-28 10:16:41 +0100

    primer commit

---
+++ src/main/java/com/neo4jbodrio/bodrio/BodrioApplication.java
@@ -0,0 +1,13 @@
+package com.neo4jbodrio.bodrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodrioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodrioApplication.class, args);
+	}
+
+}
